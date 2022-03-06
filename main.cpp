e37325fb2eeb006871da2503a964bf39d60ca7c3
Konrad C <conrisc@gmail.com>
2022-03-06 22:32:05 +0100

    Clear main.cpp

--- main.cpp
+++ main.cpp
@@ -40,24 +40,18 @@ typedef double MY_TYPE;
 #endif
 
 void usage(void) {
-	// Error function in case of incorrect command-line
 	// argument specifications
-	std::cout << "\nuseage: record N fs <duration> <device> <channelOffset>\n";
+	std::cout << "\nuseage: record N fs bf <device> <channelOffset>\n";
 	std::cout << "    where N = number of channels,\n";
-	std::cout << "    fs = the sample rate,\n";
-	std::cout << "    duration = optional time in seconds to record (default = "
-	             "8.0),\n";
+	std::cout << "    fs = the sample rate (e.g. 44100),\n";
+	std::cout << "    bf = buffer frames (e.g 2048),\n";
 	std::cout << "    device = optional device to use (default = 0),\n";
-	std::cout << "    and channelOffset = an optional channel offset on the "
-	             "device (default = 0).\n\n";
+	std::cout << "    channelOffset = an optional channel offset on the device (default = 0).\n\n";
 	// exit(0);
 }
 
 struct InputData {
-	// MY_TYPE *buffer;
-	// unsigned long bufferBytes;
-	// unsigned long totalFrames;
-	unsigned long frameCounter;
+	unsigned long bufferBytes;
 	unsigned int channels;
 };
 
@@ -67,61 +61,29 @@ Analyser analyser;
 int counter = 0;
 
 // Interleaved buffers
-int input(void * /*outputBuffer*/, void *inputBuffer,
-          unsigned int nBufferFrames, double /*streamTime*/,
-          RtAudioStreamStatus /*status*/, void *data) {
+int input(
+	void * /*outputBuffer*/,
+	void *inputBuffer,
+	unsigned int nBufferFrames,
+	double /*streamTime*/,
+	RtAudioStreamStatus /*status*/,
+	void *data
+) {
+
 	InputData *iData = (InputData *)data;
 	MY_TYPE *bufferData = (MY_TYPE *)inputBuffer;
-	// unsigned long bufferBytes = nBufferFrames * iData->channels *
-	// sizeof(MY_TYPE);
 	unsigned long length = nBufferFrames * iData->channels;
 
-	// if (counter++ % 10 == 0) {
-		// std::cout << "Data: ";
-		// for (unsigned int i = 0; i < 100; i++) {
-		// 	std::cout << bufferData[i] << ',';
-		// }
-		// std::cout << std::endl;
+	LogAverages visualization = analyser.getVisualization(bufferData, length);
+	client.send(visualization.buffer, visualization.bufferBytes);
 
-		// std::cout << nBufferFrames << std::endl;
-		// char *lol = (char*)"Fajnie"; // bad practice
-		LogAverages visualization = analyser.getVisualization(bufferData, length);
-		client.send(visualization.buffer, visualization.bufferBytes);
-		// double tmp[nBufferFrames];
-		// for (unsigned int i = 0; i < nBufferFrames; i++) {
-		// 	tmp[i] = bufferData[i];
-		// }
-		// auto fft = Aquila::FftFactory::getFft(nBufferFrames);
-		// Aquila::SpectrumType spectrum = fft->fft(tmp);
-		// Aquila::TextPlot plot("Spectrum");
-		// plot.plotSpectrum(spectrum);
-	// }
-
-	// Simply copy the data to our allocated buffer.
-	unsigned int frames = nBufferFrames;
-	// if (iData->frameCounter + nBufferFrames > iData->totalFrames) {
-	// frames = iData->totalFrames - iData->frameCounter;
-	// iData->bufferBytes = frames * iData->channels * sizeof(MY_TYPE);
-	// }
-
-	// unsigned long offset = iData->frameCounter * iData->channels;
-	// memcpy(iData->buffer + offset, inputBuffer, iData->bufferBytes);
-	iData->frameCounter += frames;
-	// std::cout<<"Frames: "<<iData->frameCounter<<std::endl;
-
-	// if (iData->frameCounter >= iData->totalFrames)
 	// 	return 2;
-
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
-	unsigned int channels = 2, fs = 30100, bufferFrames, device = 0, offset = 0;
-	double time = 8.0;
-	// FILE *fd;
+	unsigned int channels = 1, fs = 30100, bufferFrames = 2048, device = 0, offset = 0;
 
-	// minimal command-line checking
-	// if (argc < 3 || argc > 6)
 	usage();
 
 	RtAudio adc;
@@ -135,7 +97,7 @@ int main(int argc, char *argv[]) {
 	if (argc > 2)
 		fs = (unsigned int)atoi(argv[2]);
 	if (argc > 3)
-		time = (double)atof(argv[3]);
+		bufferFrames = (unsigned int)atof(argv[3]);
 	if (argc > 4)
 		device = (unsigned int)atoi(argv[4]);
 	if (argc > 5)
@@ -145,39 +107,26 @@ int main(int argc, char *argv[]) {
 	adc.showWarnings(true);
 
 	// Set our stream parameters for input only.
-	bufferFrames = 2048;
 	RtAudio::StreamParameters iParams;
 	if (device == 0)
 		iParams.deviceId = adc.getDefaultOutputDevice();
 	// iParams.deviceId = adc.getDefaultInputDevice();
 	else
 		iParams.deviceId = device;
+
 	iParams.nChannels = channels;
 	iParams.firstChannel = offset;
 
 	InputData data;
-	// data.buffer = 0;
 	try {
-		adc.openStream(NULL, &iParams, FORMAT, fs, &bufferFrames, &input,
-		               (void *)&data);
+		adc.openStream(NULL, &iParams, FORMAT, fs, &bufferFrames, &input, (void *)&data);
 	} catch (RtAudioError &e) {
 		std::cout << '\n' << e.getMessage() << '\n' << std::endl;
 		goto cleanup;
 	}
 
-	// data.bufferBytes = bufferFrames * channels * sizeof(MY_TYPE);
-	// data.totalFrames = (unsigned long)(fs * time);
-	data.frameCounter = 0;
+	data.bufferBytes = bufferFrames * channels * sizeof(MY_TYPE);
 	data.channels = channels;
-	// unsigned long totalBytes;
-	// totalBytes = data.totalFrames * channels * sizeof(MY_TYPE);
-
-	// Allocate the entire data buffer before starting stream.
-	// data.buffer = (MY_TYPE *)malloc(totalBytes);
-	// if (data.buffer == 0) {
-	// 	std::cout << "Memory allocation error ... quitting!\n";
-	// 	goto cleanup;
-	// }
 
 	try {
 		adc.startStream();
@@ -186,10 +135,6 @@ int main(int argc, char *argv[]) {
 		goto cleanup;
 	}
 
-	std::cout << "\nRecording for " << time
-	          << " seconds ... writing file 'record.raw' (buffer frames = "
-	          << bufferFrames << ")." << std::endl;
-
 	while (adc.isStreamRunning()) {
 		SLEEP(5000); // wake every 100 ms to check if we're done
 	}
@@ -197,8 +142,6 @@ int main(int argc, char *argv[]) {
 cleanup:
 	if (adc.isStreamOpen())
 		adc.closeStream();
-	// if (data.buffer)
-	// 	free(data.buffer);
 
 	return 0;
 }
